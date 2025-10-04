b6cca562a28d7f0fafa759ef9c7671cb187e83d3
Josias <rojasalcajoeljosias@gmail.com>
2025-10-03 23:14:54 -0500

    Configuración inicial: modelos, migraciones y controlador

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstudianteController;
+use App\Http\Controllers\AsistenciaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/bienvenido', function () {
-    return view('bienvenidos');
-});
+//Ruta para estudiantes
+Route::resourse('/estudiantes', EstudianteController::class);
+    
+//Rutas para asistencias
+Route::get('/asistencias', [AsistenciaController::class, 'index'])->name('asistencia.index');
+Route::post('/asistencias', [AsistenciaController::class, 'store'])->name('asistencia.store');
